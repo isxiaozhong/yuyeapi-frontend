8a7823fc3815990a6f585d8c569821d943b2dcbf
isxiaozhong <127135108+isxiaozhong@users.noreply.github.com>
2023-08-12 13:07:30 +0800

    commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import '@umijs/max';
+const Footer: React.FC = () => {
+  const defaultMessage = '蚂蚁集团体验技术部出品';
+  const currentYear = new Date().getFullYear();
+  return (
+    
+};
+export default Footer;
